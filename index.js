39bf460d83c1f562dd5ff7555ec746e45a18cdbf
Anders Evenrud <andersevenrud@gmail.com>
2018-12-15 23:00:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,319 @@
+/*!
+ * OS.js - JavaScript Cloud/Web Desktop Platform
+ *
+ * Copyright (c) 2011-2018, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ha PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+// TODO: Check selection and update toolbar UI
+// TODO: Update dialogs based on selection
+// TODO: More options
+// TODO: Localization
+// TODO: CUstom icon
+
+import osjs from 'osjs';
+import {h, app} from 'hyperapp';
+import {name as applicationName} from './metadata.json';
+import {
+  Box,
+  Menubar,
+  MenubarItem,
+  Toolbar,
+  Button,
+  Iframe
+} from '@osjs/gui';
+
+/*
+ * Base RichText template
+ */
+const template = s => `<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body contentEditable="true">${s}</body>
+</html>`;
+
+/*
+ * Custom GUI Components
+ */
+const ToolbarContainer = (props, children) => h('div', {
+  class: 'osjs-gui',
+  style: {
+    display: 'flex'
+  }
+}, children);
+
+const ColorBox = (props) => h('div', {}, [
+  h('div', {
+    style: {
+      width: '1em',
+      height: '1em',
+      background: '#000'
+    }
+  })
+]);
+
+const RichText = (props) => h(Iframe, Object.assign({
+  box: {
+    grow: 1,
+    shrink: 1,
+  }
+}, props));
+
+/*
+ * File Menu
+ */
+const createMenu = (current, actions, _) => ([
+  {label: _('LBL_NEW'), onclick: () => actions.menuNew()},
+  {label: _('LBL_OPEN'), onclick: () => actions.menuOpen()},
+  {label: _('LBL_SAVE'), disabled: !current, onclick: () => actions.menuSave()},
+  {label: _('LBL_SAVEAS'), onclick: () => actions.menuSaveAs()},
+  {label: _('LBL_QUIT'), onclick: () => actions.menuQuit()}
+]);
+
+/*
+ * Toolbar Elements
+ */
+const toolbar = [[{
+  title: 'Strong',
+  icon: 'format-text-bold',
+  command: 'bold'
+}, {
+  title: 'Italic',
+  icon: 'format-text-italic',
+  command: 'italic'
+}, {
+  title: 'Underline',
+  icon: 'format-text-underline',
+  command: 'underline'
+}, {
+  title: 'Throughline',
+  icon: 'format-text-strikethrough',
+  command: 'strikeThrough'
+}], [{
+  title: 'Left justify',
+  icon: 'format-justify-left',
+  command: 'justifyLeft'
+}, {
+  title: 'Center justify',
+  icon: 'format-justify-center',
+  command: 'justifyCenter'
+}, {
+  title: 'Right justify',
+  icon: 'format-justify-right',
+  command: 'justifyRight'
+}], [{
+  title: 'Indent less',
+  icon: 'format-indent-less',
+  command: 'indent'
+}, {
+  title: 'Indent more',
+  icon: 'format-indent-more',
+  command: 'outdent'
+}], [{
+  title: 'Foreground',
+  element: () => h(ColorBox),
+  callback: (state, actions) => actions.selectColor('foreground')
+}, {
+  title: 'Background',
+  element: () => h(ColorBox),
+  callback: (state, actions) => actions.selectColor('background')
+}, {
+  title: 'Font',
+  element: () => 'Font',
+  callback: (state, actions) => actions.selectFont()
+}]];
+
+/*
+ * Main Window Application
+ */
+const createApplication = (core, proc, basic) => ($content, win) => {
+  const _ = core.make('osjs/locale').translate;
+  const {icon} = core.make('osjs/theme');
+  const vfs = core.make('osjs/vfs');
+
+  const toolbarButtons = (state, actions) => toolbar.map(t => {
+    const buttons = t.map(b => h(Button, {
+      title: b.title,
+      icon: b.icon ? icon(b.icon) : undefined,
+      onclick: () => {
+        if (b.command) {
+          actions.textCommand(b.command);
+        } else if (b.callback) {
+          b.callback(state, actions);
+        }
+      }
+    }, b.element ? b.element() : []));
+
+    return h(ToolbarContainer, {}, buttons);
+  });
+
+  const view = (state, actions) =>
+    h(Box, {}, [
+      h(Menubar, {}, [
+        h(MenubarItem, {
+          onclick: ev => actions.menu(ev)
+        }, _('LBL_FILE'))
+      ]),
+      h(Toolbar, {}, toolbarButtons(state, actions)),
+      h(Box, {
+        grow: 1,
+        shrink: 1,
+        class: 'osjs-gui-box-styled'
+      }, h(RichText, {
+        oncreate: el => proc.emit('richtext:inited', el)
+      }))
+    ]);
+
+  return app({
+  }, {
+    save: () => state => {
+      if (proc.args.file) {
+        proc.emit('richtext:get', contents => {
+          vfs.writefile(proc.args.file, contents);
+        });
+      }
+    },
+
+    load: item => (state, actions) => {
+      vfs.readfile(item)
+        .then(contents => actions.setDocument(contents))
+        .catch(error => console.error(error)); // FIXME: Dialog
+    },
+
+    menu: ev => (state, actions) => {
+      core.make('osjs/contextmenu').show({
+        position: ev.target,
+        menu: createMenu(proc.args.file, actions, _)
+      });
+    },
+
+    textCommand: command => () => proc.emit('richtext:command', command),
+
+    setDocument: str => () => proc.emit('richtext:write', str),
+
+    selectColor: type => () => proc.emit('tool:colordialog', type),
+    selectFont: () => () => proc.emit('tool:fontdialog'),
+
+    menuNew: () => state => basic.createNew(),
+    menuOpen: () => state => basic.createOpenDialog(),
+    menuSave: () => (state, actions) => actions.save(),
+    menuSaveAs: () => state => basic.createSaveDialog(),
+    menuQuit: () => state => proc.destroy()
+  }, view, $content);
+};
+
+/*
+ * Register OS.js Application
+ */
+osjs.register(applicationName, (core, args, options, metadata) => {
+  let iframeDocument;
+
+  const title = core.make('osjs/locale')
+    .translatableFlat(metadata.title);
+
+  const proc = core.make('osjs/application', {args, options, metadata});
+
+  const win = proc.createWindow({
+    title,
+    id: 'WriterWindow',
+    icon: proc.resource(proc.metadata.icon),
+    dimension: {width: 600, height: 700}
+  })
+    .on('destroy', () => proc.destroy())
+    .on('render', (win) => win.focus())
+    .on('drop', (ev, data) => {
+      if (data.isFile && data.mime) {
+        const found = proc.metadata.mimes.find(m => (new RegExp(m)).test(data.mime));
+        if (found) {
+          basic.open(data);
+        }
+      }
+    });
+
+  const basic = core.make('osjs/basic-application', proc, win, {
+    defaultFilename: 'New Document.txt'
+  });
+
+  win.render(($content, win) => {
+    const ha = createApplication(core, proc, basic)($content, win);
+
+    basic.on('new-file', () => ha.setDocument(''));
+    basic.on('save-file', ha.save);
+    basic.on('open-file', ha.load);
+  });
+
+  proc.on('richtext:command', (command, ...args) => {
+    if (iframeDocument && command) {
+      iframeDocument.execCommand(command, ...args);
+    }
+  });
+
+  proc.on('richtext:write', str => {
+    if (iframeDocument) {
+      iframeDocument.open();
+      iframeDocument.write(template(str));
+      iframeDocument.close();
+    }
+  });
+
+  proc.on('richtext:get', cb => {
+    if (iframeDocument && cb) {
+      cb(iframeDocument.body.innerHTML);
+    }
+  });
+
+  proc.once('richtext:inited', (iframe) => {
+    iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+    proc.emit('richtext:write', '');
+
+    basic.init();
+  });
+
+  proc.on('tool:colordialog', (type) => {
+    core.make('osjs/dialog', 'color', {}, (btn, value) => {
+      if (btn === 'ok') {
+        const commandColor = type === 'foreground' ? 'foreColor' : 'hiliteColor';
+
+        proc.emit('richtext:command', commandColor, false, value.hex);
+      }
+    });
+  });
+
+  proc.on('tool:fontdialog', () => {
+    core.make('osjs/dialog', 'font', {}, (btn, value) => {
+      if (btn === 'ok') {
+        proc.emit('richtext:command', 'fontName', false, value.name);
+        proc.emit('richtext:command', 'fontSize', false, value.size);
+      }
+    });
+  });
+
+  proc.on('destroy', () => basic.destroy());
+
+  return proc;
+});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const mode = process.env.NODE_ENV || 'development';
+const minimize = mode === 'production';
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode,
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'index.js'),
+  ],
+  optimization: {
+    minimize,
+  },
+  externals: {
+    osjs: 'OSjs'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      'icon.png'
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!@osjs)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
