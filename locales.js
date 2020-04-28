067be0ea470ee85b88aeec56a3d0df2bae7d7d9a
Rutoraipf <64479921+Rutoraipf@users.noreply.github.com>
2020-04-28 20:16:42 +0200

    Internationalization of Writer Application (#6)
    Co-authored-by: Rutorai <rutorai.pf@yandex.com>

---
+++ locales.js
@@ -0,0 +1,29 @@
+export const en_EN = {
+  'LBL_WRITER_STRONG': 'Strong',
+  'LBL_WRITER_ITALIC': 'Italic',
+  'LBL_WRITER_UNDERLINE': 'Underline',
+  'LBL_WRITER_THROUGHLINE': 'Throughline',
+  'LBL_WRITER_LEFT_JUSTIFY': 'Left justify',
+  'LBL_WRITER_CENTER_JUSTIFY': 'Center justify',
+  'LBL_WRITER_RIGHT_JUSTIFY': 'Right justify',
+  'LBL_WRITER_INDENT_LESS': 'Indent less',
+  'LBL_WRITER_INDENT_MORE': 'Indent more',
+  'LBL_WRITER_FOREGROUND': 'Foreground',
+  'LBL_WRITER_BACKGROUND': 'Background',
+  'LBL_WRITER_FONT': 'Font'
+};
+
+export const fr_FR = {
+  'LBL_WRITER_STRONG': 'Gras',
+  'LBL_WRITER_ITALIC': 'Italique',
+  'LBL_WRITER_UNDERLINE': 'Souligner',
+  'LBL_WRITER_THROUGHLINE': 'Barrer',
+  'LBL_WRITER_LEFT_JUSTIFY': 'Aligner à gauche',
+  'LBL_WRITER_CENTER_JUSTIFY': 'Centrer',
+  'LBL_WRITER_RIGHT_JUSTIFY': 'Aligner à droite',
+  'LBL_WRITER_INDENT_LESS': 'Réduire l\'indentation',
+  'LBL_WRITER_INDENT_MORE': 'Augmenter l\'indentation',
+  'LBL_WRITER_FOREGROUND': 'Premier plan',
+  'LBL_WRITER_BACKGROUND': 'Arrière plan',
+  'LBL_WRITER_FONT': 'Police'
+};
